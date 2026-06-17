989094e7ce0db6f5e22d14d5934223846f8437d5
Caio Repo <caiomarcelinog@gmail.com>
2026-06-16 22:52:42 -0300

    first commit

---
+++ src/test/java/com/gerencidor/estoque/EstoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gerencidor.estoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstoqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
